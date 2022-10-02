c5e2b9b195bd17991949dac9cda6924a5175a80d
NastyaGavrilova <gavrilovan61@gmail.com>
2022-10-02 22:46:28 +0300

    change scss styles to tailwind css styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from "./store/store";
 import App from './App';
+import "./styles/index.css";
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <Provider store = {store}>
+  <Provider store={store}>
     <App />
   </Provider>,
 );
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,tsx,ts}"],
+  theme: {
+    colors: {
+      grey: "#999999",
+      borderC: "#2c4369fe",
+      white: "#ffffff",
+      black: "#000000",
+      item: "#e8eaf5",
+      modal: "rgba(0, 0, 0, 0.5)"
+
+    },
+  },
+  plugins: [],
+}
